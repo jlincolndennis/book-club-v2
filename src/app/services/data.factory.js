334c2cd121ab592e47ef80b793570acd1dd937bc
Lincoln Dennis <j.lincoln.dennis@gmail.com>
2017-06-13 19:20:49 -0600

    hardwire june update

--- src/app/services/data.factory.js
+++ src/app/services/data.factory.js
@@ -8,21 +8,21 @@
 
   function bookClubDataFactory() {
     const _currentBook = {
-      title: 'The Immortal Life of Henrietta Lacks',
-      author: 'Rebecca Skloot',
-      synopsis: 'Her name was Henrietta Lacks, but scientists know her as HeLa. She was a poor black tobacco farmer whose cells—taken without her knowledge in 1951—became one of the most important tools in medicine, vital for developing the polio vaccine, cloning, gene mapping, and more. Henrietta\'s cells have been bought and sold by the billions, yet she remains virtually unknown, and her family can\'t afford health insurance. This phenomenal New York Times bestseller tells a riveting story of the collision between ethics, race, and medicine; of scientific discovery and faith healing; and of a daughter consumed with questions about the mother she never knew.',
-      imgUrl: 'http://gallery.tinyletterapp.com/a3dd8fb87b24c5e4dc54f5e65d6794333240b284/images/6c9263c0-51f1-4471-89a3-2e96284d908d.jpg',
-      dplUrl: 'https://catalog.denverlibrary.org/search/searchresults.aspx?ctx=1.1033.0.0.6&type=Default&term=the%20immortal%20life%20of%20henrietta%20lacks&by=KW&sort=RELEVANCE&limit=TOM=*&query=&page=0&searchid=1',
-      amzUrl: 'https://www.amazon.com/Immortal-Life-Henrietta-Lacks/dp/1400052181',
-      month: 'May',
+      title: 'Gun Machine',
+      author: 'Warren Ellis',
+      synopsis: 'After a shootout claims the life of his partner in a condemned tenement building on Pearl Street, Detective John Tallow unwittingly stumbles across an apartment stacked high with guns. When examined, each weapon leads to a different, previously unsolved murder. Confronted with the sudden emergence of hundreds of unsolved homicides, Tallow discovers that he\'s walked into a veritable deal with the devil. An unholy bargain that has made possible the rise of some of Manhattan\'s most prominent captains of industry. A hunter who performs his deadly acts as a sacrifice to the old gods of Manhattan and who may, quite simply, be the most prolific murderer in New York City\'s history.',
+      imgUrl: 'app/images/gun-machine.jpg',
+      dplUrl: 'http://www.worldcat.org/oclc/870342597',
+      amzUrl: 'https://www.amazon.com/Gun-Machine-Warren-Ellis/dp/0316187410/ref=sr_1_1?ie=UTF8&qid=1497316209&sr=8-1&keywords=gun+machine',
+      month: 'June',
       year: 2017,
-      choosenBy: 'Bridget'
+      choosenBy: 'Christina'
     };
 
     const _nextMeeting = {
-      date: new Date(2017, 5, 5),
+      date: new Date(2017, 6, 10),
       time: '7:00pm',
-      message: 'If you\'ve never been to a meeting, but you haven\'t read the book, you are still more than welcome - nay, encouraged! - to attend. It could give you a chance to meet everyone, assess the general vibe, etc. Also, it is the quickest way to find out the next book we\'ll be reading ....since sometimes it takes me a few days to remember to send out an email to the group at large.'
+      message: 'If you\'ve never been to a meeting, even if you haven\'t read the book, you are still more than welcome - nay, encouraged! - to attend.  Come meet everyone, assess the general vibe, etc. Also, it is the quickest way to find out the next book we\'ll be reading ....since sometimes it takes me a few days to remember to send out an email to the group at large.'
     };
 
     return {getCurrentBook, getNextMeeting};
