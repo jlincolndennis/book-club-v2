3c889acb6b81317f971908d64c775f51d0516748
Lincoln Dennis <j.lincoln.dennis@gmail.com>
2017-07-14 09:13:59 -0600

    july hardwire update

--- src/app/services/data.factory.js
+++ src/app/services/data.factory.js
@@ -8,19 +8,19 @@
 
   function bookClubDataFactory() {
     const _currentBook = {
-      title: 'Gun Machine',
-      author: 'Warren Ellis',
-      synopsis: 'After a shootout claims the life of his partner in a condemned tenement building on Pearl Street, Detective John Tallow unwittingly stumbles across an apartment stacked high with guns. When examined, each weapon leads to a different, previously unsolved murder. Confronted with the sudden emergence of hundreds of unsolved homicides, Tallow discovers that he\'s walked into a veritable deal with the devil. An unholy bargain that has made possible the rise of some of Manhattan\'s most prominent captains of industry. A hunter who performs his deadly acts as a sacrifice to the old gods of Manhattan and who may, quite simply, be the most prolific murderer in New York City\'s history.',
-      imgUrl: 'app/images/gun-machine.jpg',
-      dplUrl: 'http://www.worldcat.org/oclc/870342597',
-      amzUrl: 'https://www.amazon.com/Gun-Machine-Warren-Ellis/dp/0316187410/ref=sr_1_1?ie=UTF8&qid=1497316209&sr=8-1&keywords=gun+machine',
-      month: 'June',
+      title: 'Lonesome Lies Before Us',
+      author: 'Don Lee',
+      synopsis: 'Yadin Park is a talented alt-country musician whose career has floundered―doomed first by his homely looks and lack of stage presence and then by a progressive hearing disorder. His girlfriend, Jeanette Matsuda, might have been a professional photographer but for a devastating heartbreak in her teens. Now Yadin works for Jeanette\'s father\'s carpet-laying company in California while Jeanette cleans rooms at a local resort. When Yadin\'s former lover and musical partner, the celebrated Mallory Wicks, comes back into his life, all their most private hopes and desires are exposed, their secret fantasies about love and success put to the test. Drawn to the music of indie singer-songwriters like Will Johnson, who helped shape the lyrics in this book, Don Lee has written a novel that unforgettably captures America’s deepest yearnings. Beautifully sad and laced with dark humor, Lonesome Lies Before Us is a profound, heartfelt romance, a soulful and memorable song.',
+      imgUrl: 'app/images/lonesome-lies.jpg',
+      dplUrl: 'http://www.worldcat.org/title/lonesome-lies-before-us-a-novel/oclc/959885744&referer=brief_results',
+      amzUrl: 'https://www.amazon.com/Lonesome-Lies-Before-Us-Novel/dp/0393608816/ref=sr_1_1?ie=UTF8&qid=1500044641&sr=8-1&keywords=lonesome+lies+before+us',
+      month: 'July',
       year: 2017,
-      choosenBy: 'Christina'
+      choosenBy: 'Keith'
     };
 
     const _nextMeeting = {
-      date: new Date(2017, 6, 10),
+      date: new Date(2017, 7, 14),
       time: '7:00pm',
       message: 'If you\'ve never been to a meeting, even if you haven\'t read the book, you are still more than welcome - nay, encouraged! - to attend.  Come meet everyone, assess the general vibe, etc. Also, it is the quickest way to find out the next book we\'ll be reading ....since sometimes it takes me a few days to remember to send out an email to the group at large.'
     };
